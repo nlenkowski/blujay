8f19bd7aec71af21801fb406e75b260fbe4c89c9
Nathan Lenkowski <nathan@littlebiglab.com>
2014-05-27 01:18:25 +0200

    Added some javascript utilities Updated path to lbl-devtools Added responsive video support via fitvids

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
-/*
+/**
  * Main scripts
  */
 
 (function($) {
 
+    /**
+     * Utilities
+     */
+
+    // Load devtools | https://github.com/nlenkowski/lbl-devtools
+    $.getScript('http://littlebiglab.com/devtools/devtools.js', function() {
+        loadDevTools();
+    });
+
+    // Enable safe logging
+    function log(msg) {
+        if (this.console) {
+            console.log(msg);
+        }
+    }
+
 })(jQuery);
-
-/*
- * Document ready
- */
-
-jQuery(document).ready(function() {
-
-    // Load lbl-devtools
-    loadDevTools();
-
-});
