69874ab41b05258b337d34dd574209d3b0a6d0f2
Nathan Lenkowski <nathan@littlebiglab.com>
2015-02-21 02:33:50 +0100

    Updated favicon

--- header.php
+++ header.php
@@ -9,15 +9,13 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="utf-8">
-    <title><?php wp_title( '|', true, 'right' ); ?><?php echo get_bloginfo('name'); ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php wp_title( '|', true, 'right' ); ?><?php echo get_bloginfo('name'); ?></title>
+    <meta name="description" content="<?php echo get_bloginfo('description'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?php echo THEMEDIR ?>/favicon.ico" />
-    <link rel="apple-touch-icon-precomposed" href="<?php echo THEMEDIR ?>/favicon-152.png">
-    <meta name="msapplication-TileImage" content="<?php echo THEMEDIR ?>/favicon-144.png">
-    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <link rel="apple-touch-icon" href="<?php echo THEMEDIR ?>/apple-touch-icon.png">
     <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
-
     <?php wp_head(); ?>
 </head>
 
