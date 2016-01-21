6293fce38f3154c87fb8e1bbe77832fdc04d3381
Nathan Lenkowski <nathan@littlebiglab.com>
2016-01-21 16:01:51 +0100

    Renamed asset directories

---
+++ assets/scripts/main.js
@@ -0,0 +1,20 @@
+/**
+ * Main Scripts
+ */
+
+(function($) {
+
+    "use strict";
+
+    /**
+     * Utilities
+     */
+
+    // Safe logging
+    function log(msg) {
+        if (window.console) {
+            console.log(msg);
+        }
+    }
+
+})(jQuery);
