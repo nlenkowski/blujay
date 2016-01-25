5f0d237e974e7cd991880a5d3dd0883ec3a8f3f3
Nathan Lenkowski <nathan@littlebiglab.com>
2016-01-26 00:52:13 +0100

    Removed activation and tgmpa functionality

--- functions.php
+++ functions.php
@@ -1,29 +1,25 @@
 <?php
 /**
- * Functions loader
+ * Blujay includes
  *
  * @package bbln_bootstrap
  */
 
 // Theme functions
-$bbln_boostrap_includes = array(
-    '/lib/init.php',        // Register constants, menus, sidebars, widget areas, etc
-    '/lib/activation.php',  // Theme activation
-    '/lib/tgmpa.php',       // Plugin activation
-    '/lib/plugins.php',     // Plugins to automatically install
-    '/lib/shortcodes.php',  // Shortcodes
+$blujay_includes = array(
+    '/lib/setup.php',       // Register constants, menus, sidebars, widget areas, etc
+    '/lib/assets.php',      // Load scripts, styles and fonts
+    '/lib/shortcodes.php',  // Custom shortcodes
     '/lib/utilities.php',   // Utility scripts
-    '/lib/scripts.php',     // Enqueue javascripts
-    '/lib/styles.php',      // Enqueue styles and fonts
-    '/lib/custom.php'       // Custom theme functions
+    '/lib/custom.php'       // Your custom theme functions go here
 );
 
-foreach ($bbln_boostrap_includes as $file) {
-  if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'bbln_boostrap'), $file), E_USER_ERROR);
-  }
+foreach ($blujay_includes as $file) {
+    if (!$filepath = locate_template($file)) {
+        trigger_error(sprintf(__('Error locating %s for inclusion', 'blujay'), $file), E_USER_ERROR);
+    }
 
-  require_once $filepath;
+    require_once $filepath;
 }
 unset($file, $filepath);
 ?>
