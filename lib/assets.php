7cf92eb7e2edaa6263e19ff5438539227d717e1d
Nathan Lenkowski <nathan@littlebiglab.com>
2016-01-27 18:07:23 +0100

    General theme style improvements

--- lib/assets.php
+++ lib/assets.php
@@ -6,13 +6,20 @@
 // Load scripts
 function load_scripts() {
     
-    wp_enqueue_script('main-scripts', DISTDIR . '/main.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_script( 'main-scripts', DISTDIR . '/main.min.js', array('jquery'), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
 // Load styles
 function load_styles() {
     
-    wp_enqueue_style('main-styles', DISTDIR . '/main.min.css');
+    wp_enqueue_style( 'main-styles', DISTDIR . '/main.min.css', false );
 }
 add_action( 'wp_enqueue_scripts', 'load_styles' );
+
+// Load web fonts
+function load_google_fonts() {
+
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700', false ); 
+}
+add_action( 'wp_enqueue_scripts', 'load_google_fonts' );
