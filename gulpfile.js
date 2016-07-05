db718d49bed1a9b3f1ab314db23b47854fff3767
Nathan Lenkowski <nathan@littlebiglab.com>
2016-07-05 13:54:53 +0200

    Improve build process
    Added support for external config
    Added source map generation to styles and scripts tasks
    Added ES6 / Babel support to scripts task
    Switched linter from jshint to eshint
    Added images and fonts tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,144 +1,164 @@
-// Globals
+// Plugins
 var argv         = require('minimist')(process.argv.slice(2));
 var autoprefixer = require('gulp-autoprefixer');
+var babel        = require('gulp-babel');
 var browserSync  = require('browser-sync');
+var changed      = require('gulp-changed');
 var concat       = require('gulp-concat');
 var cssnano      = require('gulp-cssnano');
 var del          = require('del');
+var eslint       = require('gulp-eslint');
+var flatten      = require('gulp-flatten');
 var gulp         = require('gulp');
 var gulpif       = require('gulp-if');
-var jshint       = require('gulp-jshint');
+var imagemin     = require('gulp-imagemin');
+var imageminJpg  = require('imagemin-jpeg-recompress');
+var imageminPng  = require('imagemin-pngquant');
 var notify       = require('gulp-notify');
 var plumber      = require('gulp-plumber');
 var rename       = require('gulp-rename');
 var runSequence  = require('run-sequence');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
-var stripDebug   = require('gulp-strip-debug');
 var uglify       = require('gulp-uglify');
 
-// Config
-var config = {
+// Get project config
+var config       = require('./config.json'),
+    paths        = config.paths,
+    dependencies = config.dependencies;
 
-    // Use BrowserSync (http://browsersync.io) to proxy your dev server and synchronize code changes across devices.
-    // Specify the hostname of your dev server here.
-    devUrl: 'http://blujay.dev',
-
-    // Configure your asset and distrubution paths
-    assetsPath: 'assets/',
-    distPath: 'dist/'
-};
-
-// CLI Options
+// Command line options
 var enabled = {
 
-    // Compile assets for production when running 'gulp --production'
-    // Strips debugging statements and disables source map generation
-    production : argv.production
+    // Enable production build
+    production: argv.production
 };
 
-// ### JSHint
-// 'gulp jshint' - Lints project scripts
-gulp.task('jshint', function() {
+// Custom error handler to send native notifications
+var onError = function(err) {
+    notify.onError({
+        title: "Gulp",
+        message: "<%= error.message %>"
+    })(err);
+    this.emit('end');
+};
+
+var plumberOptions = {
+    errorHandler: onError,
+};
+
+//
+// Tasks
+//
+
+// ## Scripts
+// 'gulp scripts' - Lints, combines, minifies and adds source maps for scripts
+gulp.task('scripts', ['lint'], function() {
+    return gulp.src(dependencies.scripts)
+        .pipe(plumber(plumberOptions))
+        .pipe(gulpif(!enabled.production, sourcemaps.init()))
+        .pipe(concat('main.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(babel({
+			presets: ['es2015']
+		}))
+        .pipe(uglify())
+        .pipe(gulpif(!enabled.production, sourcemaps.write()))
+        .pipe(gulp.dest(paths.dist + 'scripts'))
+        .pipe(browserSync.stream());
+});
+
+// ## Styles
+// 'gulp styles' - Compiles, autoprefixes, minifies and adds source maps for styles
+gulp.task('styles', function() {
+    return gulp.src(dependencies.styles)
+        .pipe(plumber(plumberOptions))
+        .pipe(gulpif(!enabled.production, sourcemaps.init()))
+        .pipe(concat('main.css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sass({
+            outputStyle: 'nested',
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(cssnano())
+        .pipe(gulpif(!enabled.production, sourcemaps.write()))
+        .pipe(gulp.dest(paths.dist + 'styles'))
+        .pipe(browserSync.stream());
+});
+
+// ## Images
+// 'gulp images' - Optimizes image assets and outputs to dist
+gulp.task('images', function() {
+    return gulp.src(paths.assets + 'images/**/*')
+        .pipe(changed(paths.dist + 'images'))
+        .pipe(imagemin(
+            [imageminPng(), imageminJpg()],
+            {verbose: true}
+        ))
+        .pipe(gulp.dest(paths.dist + 'images'))
+        .pipe(browserSync.stream());
+});
+
+// ## Fonts
+// `gulp fonts` - Outputs font assets to dist in a flattened directory structure
+gulp.task('fonts', function() {
     return gulp.src([
-        config.assetsPath + 'scripts/main.js'
-    ])
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(notify(function (file) {
-        if (file.jshint.success) {
-            return false;
-        }
-        var errors = file.jshint.results.map(function (data) {
-            if (data.error) {
-                return '(' + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
-            }
-        }).join('\n');
-        return file.relative + ' (' + file.jshint.results.length + ' errors)\n' + errors;
-    }));
+        paths.assets + 'fonts/**/*.eot',
+        paths.assets + 'fonts/**/*.svg',
+        paths.assets + 'fonts/**/*.ttf',
+        paths.assets + 'fonts/**/*.woff',
+        paths.assets + 'fonts/**/*.woff2'
+        ])
+        .pipe(flatten())
+        .pipe(gulp.dest(paths.dist + 'fonts'))
+        .pipe(browserSync.stream());
 });
 
-// ### Scripts
-// 'gulp scripts' - Lints, combines and minimizes app and vendor scripts
-gulp.task('scripts', ['jshint'], function() {
-
-    // App
-    return gulp.src([
-        config.assetsPath + 'scripts/main.js'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('main.js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulpif(enabled.production, stripDebug()))
-    .pipe(uglify())
-    .pipe(gulpif(!enabled.production, sourcemaps.write()))
-    .pipe(gulp.dest(config.distPath + '/scripts/')),
-
-    // Vendor
-    gulp.src([
-        config.assetsPath + 'scripts/vendor/*.js',
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('vendor.js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulpif(!enabled.production, sourcemaps.write()))
-    .pipe(gulp.dest(config.distPath + '/scripts/'));
+// ## Lint
+// 'gulp lint' - Lints main project scripts with eslint
+gulp.task('lint', function() {
+    return gulp.src(paths.assets + 'scripts/**/*.js')
+        .pipe(plumber(plumberOptions))
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
-// ### Styles
-// 'gulp styles' - Compiles, combines, and autoprefixes styles
-gulp.task('styles', function () {
-    return gulp.src([
-        config.assetsPath + 'styles/main.scss'
-    ])
-    .pipe(plumber({
-        errorHandler: function(err) {
-            notify().write(err);
-            console.log(err);
-            this.emit('end');
-        }
-    }))
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle: 'nested'
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions']
-    }))
-    .pipe(cssnano())
-    .pipe(gulpif(!enabled.production, sourcemaps.write()))
-    .pipe(gulp.dest(config.distPath + '/styles/'))
-    .pipe(browserSync.stream());
-});
+// ## Clean
+// 'gulp clean' - Deletes the dist directory
+gulp.task('clean', del.bind(null, [paths.dist]));
 
-// ### BrowserSync Init
-gulp.task('browsersync', function() {
-    browserSync.init({
-        proxy: config.devUrl
-    });
-});
-
-// ### BrowserSync Reload
-gulp.task('reload', function () {
+// ## Reload
+// 'gulp reload' - Forces a manual browser reload
+gulp.task('reload', function() {
     browserSync.reload();
 });
 
-// ### Clean
-// 'gulp clean' - Deletes the build folder
-gulp.task('clean', del.bind(null, [config.distPath]));
-
-// ### Watch
-// 'gulp watch' - When a modification is made to a watched asset the changes are immediately injected into the page using BrowserSync.
-gulp.task('watch', ['scripts', 'styles', 'browsersync'], function () {
-    gulp.watch(['assets/styles/**/*'], ['styles']);
-    gulp.watch(['assets/scripts/**/*'], ['scripts', 'reload']);
+// ## Watch
+// `gulp watch` - Use BrowserSync to proxy your local development server and
+// synchronize code changes across devices. Specify your development server
+// hostname in config.json. See http://browsersync.io for details.
+gulp.task('watch', function() {
+    browserSync.init({
+        proxy: config.devUrl,
+        snippetOptions: {
+            whitelist: ['/wp-admin/admin-ajax.php'],
+            blacklist: ['/wp-admin/**']
+        }
+    });
     gulp.watch(['**/*.php'], ['reload']);
+    gulp.watch([paths.assets + 'scripts/**/*'], ['scripts']);
+    gulp.watch([paths.assets + 'styles/**/*'], ['styles']);
+    gulp.watch([paths.assets + 'fonts/**/*'], ['fonts']);
+    gulp.watch([paths.assets + 'images/**/*'], ['images']);
 });
 
-// ### Gulp
-// 'gulp' - Run a complete build. To compile for production run 'gulp --production'
-gulp.task('default', function () {
-    runSequence('clean', 'styles', 'scripts', 'reload');
+// ## Gulp
+// 'gulp' - Compiles and optimizes project assets
+// 'gulp --production' - Compiles assets for production (disables source maps)
+gulp.task('default', function() {
+    require('gulp-stats')(gulp);
+    runSequence('clean', 'scripts', 'styles', 'fonts', 'images');
 });
