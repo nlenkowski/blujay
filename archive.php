fe5a019c36600b11879df5fb36959e61bc6cef26
Nathan Lenkowski <nathan@littlebiglab.com>
2016-07-06 15:54:44 +0200

    Add search and archive templates

---
+++ archive.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Displays archive entries
+ */
+?>
+
+<?php get_template_part( 'partials/header' ); ?>
+
+<main class="main">
+
+    <?php if ( have_posts() ) : ?>
+
+        <header class="entries-header">
+            <h1 class="entries-title"><?php the_archive_title(); ?></h1>
+        </header>
+
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'partials/content', 'excerpt' ); ?>
+        <?php endwhile; ?>
+
+        <?php if ($wp_query->max_num_pages > 1) : ?>
+            <?php get_template_part( 'partials/pager' ); ?>
+        <?php endif; ?>
+
+    <?php else : ?>
+        <?php get_template_part( 'partials/content', 'none' ); ?>
+    <?php endif; ?>
+
+</main>
+
+<?php get_template_part( 'partials/sidebar' ); ?>
+
+<?php get_template_part( 'partials/footer' ); ?>
