5d2753be147b7b53839246c23891d394a40de6fe
Nathan Lenkowski <nathan@littlebiglab.com>
2016-07-06 23:56:13 +0200

    Improve code readability

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+    <link rel="shortcut icon" href="<?php echo THEMEDIR ?>/favicon.ico">
+    <link rel="apple-touch-icon" href="<?php echo THEMEDIR ?>/apple-touch-icon.png">
     <?php wp_head(); ?>
 </head>
 
