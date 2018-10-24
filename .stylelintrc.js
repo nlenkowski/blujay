61cd9635145c513541371d31c2e9aa2246a33e5b
Nathan Lenkowski <nathan@littlebiglab.com>
2018-10-24 14:06:39 +0200

    Add stylelint rules

---
+++ .stylelintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  rules: {
+    "no-empty-source": null,
+    "no-descending-specificity": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: [
+          "extend",
+          "at-root",
+          "debug",
+          "warn",
+          "error",
+          "if",
+          "else",
+          "for",
+          "each",
+          "while",
+          "mixin",
+          "include",
+          "content",
+          "return",
+          "function"
+        ]
+      }
+    ]
+  }
+};
