54afff23522ca18efdf1333136e9c5eb9d58d53b
Nathan Lenkowski <nathan@littlebiglab.com>
2018-10-24 16:01:43 +0200

    Add Mix rule to copy fonts and images assets to dist

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,8 @@ const mix = require("laravel-mix");
 mix
   .js("assets/scripts/main.js", "dist/scripts")
   .sass("assets/styles/main.scss", "dist/styles")
+  .copyDirectory("assets/fonts", "dist/fonts")
+  .copyDirectory("assets/images", "dist/images")
   .browserSync({
     proxy: "https://blujay.test",
     snippetOptions: {
