6b14af71d6c44d4049f1e216a972f5d3648b9b6c
Nathan Lenkowski <nathan@littlebiglab.com>
2019-02-20 14:46:01 +0100

    Enable Browsersync and source maps for development builds only

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,22 @@
-const mix = require("laravel-mix");
+// eslint-disable-next-line no-undef
+const mix = require('laravel-mix');
 
 mix
-  .js("assets/scripts/main.js", "dist/scripts")
-  .sass("assets/styles/main.scss", "dist/styles")
-  .copyDirectory("assets/fonts", "dist/fonts")
-  .copyDirectory("assets/images", "dist/images")
-  .browserSync({
-    proxy: "https://blujay.test",
-    files: ["dist/styles/**/*.css", "dist/scripts/**/*.js", "**/*.php"],
+  .js('assets/scripts/main.js', 'dist/scripts')
+  .sass('assets/styles/main.scss', 'dist/styles')
+  .copyDirectory('assets/fonts', 'dist/fonts')
+  .copyDirectory('assets/images', 'dist/images');
+
+// Enable Browsersync and source maps for development builds only
+if (!mix.inProduction()) {
+  mix.webpackConfig({ devtool: 'inline-source-map' });
+  mix.browserSync({
+    proxy: 'https://blujay.test',
+    open: false,
+    notify: false,
+    files: ['dist/styles/**/*.css', 'dist/scripts/**/*.js', '**/*.php'],
     snippetOptions: {
-      ignorePaths: ["admin/**"]
-    }
+      ignorePaths: ['admin/**'],
+    },
   });
+}
